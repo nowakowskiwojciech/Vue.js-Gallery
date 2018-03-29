e4d793e612941bd35128609700725f754c020d47
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2018-03-29 17:14:34 +0200

    animation fixes

--- src/components/category.vue
+++ src/components/category.vue
@@ -1,91 +1,96 @@
 <template>
-    <div>
-        <div class='display'  >
-              <photo v-for="photo in photos" :styl ="shift" :url="photo" :key="photo">
-              </photo>
-        </div>
-          <div style='display:flex; justify-content:center;'>
-            <span style='margin:20px; cursor:pointer; font-weight:bold' @mouseover="startAnimate" @mouseleave="stop"><-</span>
-            <span style='margin:20px; cursor:pointer; font-weight:bold' @mouseover="startAnimate" @mouseleave="stop">-></span>
-          </div>
+  <div>
+    <div class='display'>
+      <photo v-for="photo in photos" :styl="shift" :url="photo" :key="photo">
+      </photo>
     </div>
+    <div style='display:flex; justify-content:center;'>
+      <span style='margin:20px; cursor:pointer; font-weight:bold' @mouseover="startAnimate" @mouseleave="stop"><-</span>
+      <span style='margin:20px; cursor:pointer; font-weight:bold' @mouseover="startAnimate" @mouseleave="stop">-></span>
+    </div>
+  </div>
 </template>
 
 <script>
-import photo from '../components/photo'
-
-export default {
-  props:['show', 'photos'],
-  name: 'Category',
-  components: {
-    photo
-  },
-  data () {
-    return {
-      shift: 0,
-      mouseX:0,
-      x:0,
-      friction:0,
-      can: true,
-      can2:true   }
-  },
-  methods: {
-    moveEl(){
-      this.x+=this.friction;
-      this.shift = this.x+'px'
+  import photo from '../components/photo'
+  
+  export default {
+    props: ['show', 'photos'],
+    name: 'Category',
+    components: {
+      photo
     },
-    startAnimate (){
-      if(this.can == true){
-        window.var = setInterval(this.moveEl,20);
+    data() {
+      return {
+        shift: 0,
+        mouseX: 0,
+        x: 0,
+        friction: 0,
+        can: true,
+        can2: true
+      }
+    },
+    methods: {
+      moveEl() {
+        this.x += this.friction;
+        this.shift = this.x + 'px'
+      },
+      startAnimate() {
+        if (this.can == true) {
+          window.var = setInterval(this.moveEl, 20);
+          var that = this;
+          window.el = setInterval(function() {
+            that.friction += 0.2;
+            if (that.friction >= 8) {
+              clearInterval(window.el);
+            }
+          }, 30)
+          this.can = false;
+        }
+      },
+      stop() {
+        clearInterval(window.el);
+  
         var that = this;
-        window.el = setInterval(function(){
-        that.friction += 0.2;
-          if(that.friction >= 8){
-            clearInterval(window.el);
-          }
-        },30)
-        this.can = false;
+        if (this.can2 == true) {
+          window.slizg = setInterval(function() {
+            that.friction -= 0.2
+            if (that.friction <= 0) {
+              clearInterval(window.var);
+              that.can = true;
+              clearInterval(window.slizg);
+              that.can2 = true;
+              that.friction = 0;
+            }
+          }, 30)
+        }
+        this.can2 = false;
+  
       }
     }
-    ,
-    stop() {
-      clearInterval(window.el);
-
-      var that = this;
-      if(this.can2 == true){
-        window.slizg = setInterval(function(){
-          that.friction -= 0.2
-          if(that.friction <=0 ){
-            clearInterval(window.var);
-            that.can = true;
-            clearInterval(window.slizg);
-            that.can2 = true;
-            that.friction = 0;
-          }
-        },30)
-    }
-    this.can2 = false;
-
-    }
   }
-}
-
-
 </script>
+
 <style>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 2s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-
-.display {
-  white-space:nowrap;
-  max-width: 100%;
-
-}
-body {
-  overflow:hidden
-}
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 2s;
+  }
+  
+  .fade-enter,
+  .fade-leave-to
+  /* .fade-leave-active below version 2.1.8 */
+  
+  {
+    opacity: 0;
+  }
+  
+  .display {
+    white-space: nowrap;
+    max-width: 100%;
+  }
+  
+  body {
+    overflow: hidden
+  }
 </style>
--- src/components/photo.vue
+++ src/components/photo.vue
@@ -1,66 +1,63 @@
 <template>
-    <span>
-      <img @click="open" class="base" :src="imagePath(url)" :style="{left: styl}">
-      <transition  name="fade" >
-        <img  @click="close" v-if="opened" class="big" :src="imagePath(url)">
-      </transition>
-    </span>
+  <span>
+        <img @click="open" class="base" :src="imagePath(url)" :style="{left: styl}">
+        <transition  name="fade" >
+          <img  @click="close" v-if="opened" class="big" :src="imagePath(url)">
+        </transition>
+      </span>
 </template>
 
 <script>
-export default {
-  props: ['url','styl'],
-  data () {
-    return {
-      opened: false
-    }
-  },
-  methods: {
-    imagePath (url) {
-      return require('../assets/' + url + '.jpg')
+  export default {
+    props: ['url', 'styl'],
+    data() {
+      return {
+        opened: false
+      }
     },
-    open () {
-      this.opened = true
-    },
-    close () {
-      this.opened = false
+    methods: {
+      imagePath(url) {
+        return require('../assets/' + url + '.jpg')
+      },
+      open() {
+        this.opened = true
+      },
+      close() {
+        this.opened = false
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.base{
-  position: relative;
-  opacity:1;
-  width:auto;
-  height:400px;
-  margin: 10px;
-  border: 5px solid white;
-  border-radius: 10px;
-  transition: opacity 1s;
-  cursor: pointer
-}
-.base:hover {
-  opacity:0.2;
-
-}
-
-.big {
-
-  border: 5px solid white;
-  border-radius: 10px;
-  position: absolute;
-  max-height: 90%;
-  width:auto;
-  margin: auto;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  z-index: 1000;
-
-}
-
+  .base {
+    position: relative;
+    opacity: 1;
+    width: auto;
+    height: 400px;
+    margin: 10px;
+    border: 5px solid white;
+    border-radius: 10px;
+    transition: opacity 1s;
+    cursor: pointer
+  }
+  
+  .base:hover {
+    opacity: 0.2;
+  }
+  
+  .big {
+    border: 5px solid white;
+    border-radius: 10px;
+    position: absolute;
+    max-height: 90%;
+    width: auto;
+    margin: auto;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 1000;
+  }
 </style>
